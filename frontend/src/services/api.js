87a2d2724579d012a50206c50bfce379ef0bbcdf
innama-f <innama.fatima27@gmail.com>
2026-05-17 21:48:12 +0530

    Fixed CRA env variables

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log("ENV:", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
 });
 
 // ✅ ADD THIS PART (VERY IMPORTANT)
